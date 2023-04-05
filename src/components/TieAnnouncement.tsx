71eb8005577062aaadf7b0b5f5de70c38cc844e8
Sædís Björk <saedisbjork@gmail.com>
2023-04-05 22:11:32 +0200

    Improv: Tie Announcement

---
+++ src/components/TieAnnouncement.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export function TieAnnouncement() {
+  return (
+    <h1 className={styles.title}>
+      <div>Oh no! It&apos;s a Tie! Well, I think this calls for a rematch!</div>
+    </h1>
+  );
+}
